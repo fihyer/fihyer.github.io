b837b8e3786ec9dc6d9800d91c94fa2a6482ac3e
runze <fihyer@me.com>
2021-08-13 16:29:55 +0800

    Site updated: 2021-08-13 16:29:55

--- js/utils.js
+++ js/utils.js
@@ -420,3 +420,16 @@ NexT.utils = {
     });
   }
 };
+
+
+// function that get html footnote contents
+$(document).ready(function(){
+    $(".tooltip-turnright").each(function(){
+        var id = $(this).children("a").attr("href").substr(1);
+            footnote = $(document.getElementById(id));
+            outer_wrapper = $("<span>",{"class":"tooltip-item"});
+            inner_wrapper = $("<span>",{"class":"tooltip-content"});
+            // footnote.find(".footnote-backref").remove();
+            $(this).append(outer_wrapper.append(inner_wrapper.html(footnote.html())));
+    });
+});
