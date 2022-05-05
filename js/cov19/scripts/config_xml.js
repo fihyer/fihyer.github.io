cab01b4c657a5eaa269c0b397c7b75ba3f59ba6d
fihyer <fihyer@me.com>
2022-05-05 17:28:37 +0800

    Site updated: 2022-05-05 17:28:34

---
+++ js/cov19/scripts/config_xml.js
@@ -0,0 +1,58 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2022-04-27 10:17:54 AM" dc:source="Camtasia,21.0.17,enu" dc:title="高一每日真实核酸检测汇总打印说明" tscDM:firstFrame="高一每日真实核酸检测汇总打印说明_First_Frame.png" tscDM:originId="5A7962DF-42B6-4A50-8CB1-33BC2C0B1600" tscDM:project="Untitled Project">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="258333"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="768" stDim:w="1366"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Caption" xmpDM:frameRate="f1000" xmpDM:trackName="Captioning" stFnt:fontFamily="Arial" tscDM:fontSize="39" tscDM:bgOpacity="0.750000" tscDM:position="overlay">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="4167" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 首先我们要进入学校的VOD}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4467" xmpDM:startTime="8167" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 大家也可以通过直接将下面这个地址输入到电脑文件夹地址栏  \\\\\\\\192.168.10.5\\\\vod\\\\高一1部\\\\#2022_20%核酸抽检}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="12633" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 或者你也可以点击这里复制VOD地址}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="7367" xmpDM:startTime="23667" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 找到#2022_20%核酸抽检文件夹，如果不好找可以按照文件类型排序}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="7867" xmpDM:startTime="146033" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 表中只需要将学生姓名以及对应身份证号码粘贴为数值，其余信息不动}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="1900" xmpDM:startTime="181900" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 表格打印出来后“备注”栏可以作为学生签字栏使                      声音卡没录上 ：）}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:duration="4000" xmpDM:startTime="183833" tscDM:valign="bottom" tscDM:halign="center"><xmpDM:name><rdf:Alt><rdf:li xml:lang="en-US">{\\rtf1 如果当天核酸抽检学生多，还是只在姓名和身份证号列填上信息就行}</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tsc:fgColor xmpG:red="255" xmpG:green="255" xmpG:blue="255"/><tsc:bgColor xmpG:red="0" xmpG:green="0" xmpG:blue="0"/></rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="731D54AF-01D5-4327-B5FD-BAE62DDD158D" tscIQ:authoredEmail="QCdcCY18K0OJv2xg/V0sPGMcI7xPq79RFEv2BuE87lOUcblYQB3tXePCIgbeEG5lXShpz53Vtocf\
+D763uVf3vRYLtj3lPnChgCqnOzXytveMKH29ODb26bZrny7Zz28xFUWT7JslUqir94+sukZxES2w\
+0xZF36+4RVMI+sP9NwGncX+B50cmtHsTkd3Gl19xwfGoJlREsb92LN09vHm54qyId7AKl241jj5M\
+SjLV8y1SAWZOp3cF2z/KhjZqpCufqvPsAV+gt3Gp8psyyEkqtC6O/RbivtYOLxzE1hUnmQzPAgyD\
+vxH47ot96bpaHRbDnh/g7Hk4woTErvzu55zkhg==" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="API" tscIQ:allowSkipQuiz="0" tscIQ:clientId="E7885279-24D1-4E90-AF2A-EF9A7A4E075B" tscIQ:hideReplay="0" tscIQ:quizHash="76ec1e89b25fa5a9d32f321bc914e785">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="16633" tscIQ:feedback="1" tscIQ:questionSetName="Quiz 1"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>VOD核酸抽检地址: \\\\192.168.10.5\\vod\\高一1部\\#2022_20%核酸抽检</tscIQ:question><tscIQ:correctAnswer>1</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>True</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>False</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li></rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li></rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="上一个"/><rdf:li xmpDM:name="txtNext" xmpDM:value="下一个"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="点击复制VOD地址"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="OK"/><rdf:li xmpDM:name="txtReview" xmpDM:value="重播"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="查看VOD地址"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="继续播放"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="true"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="true"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="高一每日真实核酸检测汇总打印说明.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="高一每日真实核酸检测汇总打印说明_First_Frame.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
